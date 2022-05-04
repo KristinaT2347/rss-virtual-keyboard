5a0b8379f178d42d302c1719c9fb82a34df5e82a
Kristina <kristinatabula@icloud.com>
2022-05-04 17:11:22 +0300

    feat: add textarea

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const title = document.createElement('h1');
+title.textContent = 'RSS VIRTUAL KEYBOARD';
+document.body.append(title);
 
+const textArea = document.createElement('textarea');
+document.body.append(textArea);
